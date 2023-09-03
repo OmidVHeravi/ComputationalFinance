d90548d73acaf39e6ef30db1016afe6ab65b6479
Omid Heravi <omidv28@gmail.com>
2023-09-02 21:09:36 -0700

    Add files via upload

--- RLNQ/RLNQ.py
+++ RLNQ/RLNQ.py
@@ -1,82 +1,123 @@
-import os
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
-import dash_table
-import base64
-import io
-import pandas as pd
-from PIL import Image
+import cv2
 import numpy as np
-from tensorflow.keras import layers, models
-from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.python.keras import layers, models
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
 import gym
 from gym import spaces
 
-# Define your CNN model
+# ------------------------- Image Preprocessing -------------------------
+
+def resize_image(image, target_height=224, target_width=224):
+    return cv2.resize(image, (target_width, target_height))
+
+def normalize_image(image):
+    return image / 255.0
+
+def augment_image(image):
+    rotation_angle = np.random.uniform(-10, 10)
+    rows, cols, _ = image.shape
+    rotation_matrix = cv2.getRotationMatrix2D((cols/2, rows/2), rotation_angle, 1)
+    return cv2.warpAffine(image, rotation_matrix, (cols, rows))
+
+def preprocess_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = resize_image(image)
+    image = normalize_image(image)
+    image = augment_image(image)
+    return image
+
+# ------------------------- CAM Generation -------------------------
+
+def get_cam(image, model, last_conv_layer_name):
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    with tf.GradientTape() as tape:
+        conv_output_values, predictions = grad_model(image)
+        loss = predictions[:, 0]
+
+    grads_values = tape.gradient(loss, conv_output_values)
+    grads_values = tf.reduce_mean(grads_values, axis=(0, 1, 2))
+
+    cam = np.ones(conv_output_values.shape[1:], dtype=np.float32)
+    for i, w in enumerate(grads_values.numpy()):
+        cam += w * conv_output_values[0, :, :, i]
+
+    cam = cv2.resize(cam.numpy(), (224, 224))
+    cam = np.maximum(cam, 0)
+    heatmap = (cam - cam.min()) / (cam.max() - cam.min())
+
+    cam = cv2.applyColorMap(np.uint8(255 * heatmap), cv2.COLORMAP_JET)
+    output_image = cv2.addWeighted(cv2.cvtColor(image.astype('uint8'), cv2.COLOR_RGB2BGR), 0.5, cam, 1, 0)
+    return output_image
+
+# ------------------------- CNN & RL Model -------------------------
+
 def build_cnn():
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 3)))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
-    # Add more convolutional layers if needed
     model.add(layers.Flatten())
     model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(1, activation='sigmoid'))  # Binary classification
-    
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.add(layers.Dense(3, activation='softmax'))
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-# Initialize your model
-cnn_model = build_cnn()
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.model = self._build_model()
 
-# Define your trading environment
+    def _build_model(self):
+        model = models.Sequential()
+        model.add(layers.Dense(24, activation='relu', input_shape=(self.state_size,)))
+        model.add(layers.Dense(24, activation='relu'))
+        model.add(layers.Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=0.001))
+        return model
+
+# ------------------------- Custom Trading Environment -------------------------
+
 class TradingEnv(gym.Env):
     def __init__(self, data, initial_balance=10000):
         super(TradingEnv, self).__init__()
-
         self.data = data
         self.initial_balance = initial_balance
-        
-        # Define action and observation space
-        # 0: Hold, 1: Buy, 2: Sell
         self.action_space = spaces.Discrete(3)
-        
-        # Observation: CNN output + other possible trading indicators
-        self.observation_space = spaces.Box(low=0, high=1, shape=(OBSERVATION_SIZE,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=0, high=1, shape=(224, 224, 3), dtype=np.float32)
 
     def reset(self):
-        # Reset environment state
         self.balance = self.initial_balance
         self.current_step = 0
         return self.get_observation()
 
     def get_observation(self):
-        # Combine CNN output with other indicators and return
         pass
 
     def step(self, action):
-        # Implement the trading logic, return next state, reward, done, and info
         pass
 
     def render(self, mode='human'):
-        # Render the environment, if needed
         pass
 
     def close(self):
-        # Clean up environment
         pass
+
+# ------------------------- Dash App for Data Upload and Labeling -------------------------
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1("Reinforcement Learning for Trading"),
+    html.H1("CNN & RL Trading Model Trainer"),
     dcc.Upload(
-        id='upload-image',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
+        id='upload-data',
+        children=html.Div(['Drag and Drop or ', html.A('Select Images')]),
         style={
             'width': '100%',
             'height': '60px',
@@ -87,85 +128,46 @@ app.layout = html.Div([
             'textAlign': 'center',
             'margin': '10px'
         },
-        # Allow multiple files to be uploaded
         multiple=True
     ),
-    html.Div(id='output-image-upload'),
-    html.H2("Enter the label for the uploaded image(s):"),
-    dcc.Input(id='input-label', type='text', placeholder='Enter label'),
-    html.Button('Submit', id='submit-label', n_clicks=0),
-    html.Div(id='output-label'),
-    html.H2("Model Training Progress:"),
-    dcc.Graph(id='model-progress')
+    html.Div(id='label-dropdowns'),
+    html.Button('Train Model', id='train-button'),
+    dcc.Graph(id='model-graph')
 ])
 
-# Global variable to store uploaded images
-uploaded_images = []
+@app.callback(
+    Output('label-dropdowns', 'children'),
+    Input('upload-data', 'filenames')
+)
+def update_dropdowns(filenames):
+    if filenames is None:
+        return []
+    dropdowns = []
+    for fname in filenames:
+        dropdown = dcc.Dropdown(
+            options=[
+                {'label': 'Bullish Reversal', 'value': 2},
+                {'label': 'No Reversal', 'value': 1},
+                {'label': 'Bearish Reversal', 'value': 0}
+            ],
+            value=1,
+            style={'width': '50%', 'marginBottom': '10px'}
+        )
+        dropdowns.append(dropdown)
+    return dropdowns
 
-@app.callback(Output('output-image-upload', 'children'),
-              Input('upload-image', 'contents'),
-              State('upload-image', 'filename'))
-def update_output(list_of_contents, list_of_names):
-    global uploaded_images
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n) for c, n in zip(list_of_contents, list_of_names)]
-        uploaded_images = list_of_contents  # store uploaded images
-        return children
-
-def parse_contents(contents, filename):
-    content_type, content_string = contents.split(',')
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'png' in filename or 'jpg' in filename:
-            # Assume that the user uploaded an image file
-            return html.Div([
-                html.Img(src=contents),
-                html.Hr(),
-                html.Div(filename)
-            ])
-        else:
-            return html.Div([
-                'Unsupported file type uploaded'
-            ])
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-@app.callback(Output('output-label', 'children'),
-              Input('submit-label', 'n_clicks'),
-              State('input-label', 'value'))
-def update_label(n_clicks, value):
-    if n_clicks > 0:
-        # Convert uploaded images to numpy arrays and preprocess them
-        images = [preprocess_image(base64_to_np_array(img)) for img in uploaded_images]
-        # Convert label to appropriate format
-        labels = label_to_np_array(value)
-        # Train the model
-        train_model(images, labels)
-        return html.Div([
-            'Label for the uploaded image(s): {}'.format(value)
-        ])
-
-def base64_to_np_array(base64_img):
-    content_type, content_string = base64_img.split(',')
-    decoded = base64.b64decode(content_string)
-    img = Image.open(io.BytesIO(decoded))
-    return np.array(img)
-
-def preprocess_image(img):
-    # Implement your image preprocessing logic here
-    pass
-
-def label_to_np_array(label):
-    # Convert label to appropriate format
-    pass
-
-def train_model(images, labels):
-    # Implement your model training logic here
-    pass
+@app.callback(
+    Output('model-graph', 'figure'),
+    Input('train-button', 'n_clicks'),
+    State('upload-data', 'contents'),
+    State('label-dropdowns', 'value')
+)
+def train_and_visualize(n_clicks, image_contents, labels):
+    images = [preprocess_image(content.split(",")[1]) for content in image_contents]
+    return {
+        'data': [{'x': [1, 2, 3], 'y': [1, 4, 9], 'type': 'line', 'name': 'Dummy Training Data'}],
+        'layout': {'title': 'Dummy Training Visualization'}
+    }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
